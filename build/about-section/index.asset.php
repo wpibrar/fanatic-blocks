7563d5ed4864f17ca9287019bf8ee7c88f8db12d
wpibrar <wpibrar@gmail.com>
2023-08-31 07:04:01 +0500

    home-section completed

--- build/about-section/index.asset.php
+++ build/about-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '16a96fb5064e8a0a1d81');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'dadf74ab1e6ad91b8e6f');
--- build/services-boxes/index.asset.php
+++ build/services-boxes/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd6a6c7dc86aac49818d2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '088560fb88d4c04d3ad8');
