bee416e22346e569a72c32895582e5cf1619d687
ibrartt <111246165+ibrar19911@users.noreply.github.com>
2023-09-29 19:18:16 +0500

    changes in map files

--- build/expertise-block/index.asset.php
+++ build/expertise-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9ff2088e7dcbddda0760');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5b9e44582e0bccae27ef');
